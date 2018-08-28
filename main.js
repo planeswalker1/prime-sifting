d79ec218567521ad722ef4895e2df336afc42c47
Daniel Munoz <daniel.munozdev@gmail.com>
2018-08-27 22:05:41 -0700

    add starter files

---
+++ main.js
@@ -0,0 +1,10 @@
+// Prime Sifting
+// Given a number, write a method that returns all of the prime numbers less than that number.
+
+// Use the Sieve of Eratosthenes to solve it.Here's how the Sieve of Eratosthenes works to find a number up to a given number:
+
+// Create a list of numbers from 2 through n: 2, 3, 4, ..., number.
+// Initially, let prime equal 2, the first prime number.
+// Starting from prime, remove all multiples of prime from the list.
+// Increment prime by 1.
+// When you reach number, all the remaining numbers in the list are primes.
